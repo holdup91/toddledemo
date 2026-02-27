bbb73e71f0e1a3b392d52dd6b17787b230a8764c
holdup91 <holdup.design@gmail.com>
2026-02-27 08:08:00 +0100

    Update [[path]].ts

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,5 @@
 import { Hono } from 'hono'
 import { renderToReadableStream } from '@nordcraft/ssr'
-// On remonte d'un dossier pour trouver le JSON à la racine
 import projectData from '../nordcraft-export-blue_dooku_main_takin-main.json'
 
 const app = new Hono()
@@ -12,12 +11,11 @@ app.get('*', async (c) => {
       lang: "fr",
       scripts: ['https://cdn.nordcraft.io/runtime/v1/client.js']
     })
-
     return c.html(htmlStream)
   } catch (error: any) {
-    return c.text(`Erreur de rendu : ${error.message}`, 500)
+    return c.text(`Erreur : ${error.message}`, 500)
   }
 })
 
-// Cette ligne permet à Cloudflare Pages de brancher votre app Hono
+// Indispensable pour Cloudflare Pages
 export const onRequest = (context) => app.fetch(context.request, context.env)
